03723d5e401b28939a0dea12296d4928dabf02f4
s3681437 <40589362+s3681437@users.noreply.github.com>
2021-05-11 15:02:52 +0700

    hello comment

--- src/main/java/com/example/practice04/Practice04Application.java
+++ src/main/java/com/example/practice04/Practice04Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Practice04Application {
-
+	//hello
 	public static void main(String[] args) {
 		SpringApplication.run(Practice04Application.class, args);
 	}
